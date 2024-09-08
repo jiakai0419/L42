a9827c0c9db864b88a419935ac17569a2a0109fd
Kai Jia <jiakai0419@gmail.com>
2024-09-08 18:48:12 +0800

    Update PropositionsAndProofs/E5

--- L42/PropositionsAndProofs/E5.lean
+++ L42/PropositionsAndProofs/E5.lean
@@ -29,3 +29,24 @@ example : ¬(p → q) → p ∧ ¬q :=
           (λ (hq : q) => False.elim (h (λ (_ : p) => hq)))
           (λ (hnq : ¬q) => ⟨hp, hnq⟩))
       (λ (hnp : ¬p) => False.elim (h (λ (hp : p) => absurd hp hnp)))
+
+example : (p → q) → (¬p ∨ q) :=
+  λ (h : p → q) =>
+    Or.elim (em p)
+      (λ (hp : p) => Or.inr (h hp))
+      (λ (hnp : ¬p) => Or.inl hnp)
+
+example : (¬q → ¬p) → (p → q) :=
+  λ (h : (¬q → ¬p)) =>
+    λ (hp : p) =>
+      byContradiction
+        (λ (hnq : ¬q) => absurd hp (h hnq))
+
+example : p ∨ ¬p := (em p)
+
+example : (((p → q) → p) → p) :=
+  λ (h : ((p → q) → p)) =>
+    Or.elim (em p)
+      (λ (hp : p) => hp)
+      (λ (hnp : ¬p) =>
+        h (λ (hp : p) => absurd hp hnp))
