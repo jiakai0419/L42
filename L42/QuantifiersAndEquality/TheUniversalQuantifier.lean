28801676b245771c8b2a3969f568bc43b3323882
Kai Jia <jiakai0419@gmail.com>
2024-09-15 14:22:11 +0800

    Update: QuantifiersAndEquality/TheUniversalQuantifier

--- L42/QuantifiersAndEquality/TheUniversalQuantifier.lean
+++ L42/QuantifiersAndEquality/TheUniversalQuantifier.lean
@@ -28,3 +28,19 @@
 -- #check trans_r
 -- #check trans_r hab
 -- #check trans_r hab hbc
+
+
+variable (α : Type) (r : α → α → Prop)
+
+variable (refl_r : ∀ x, r x x)
+variable (symm_r : ∀ {x y}, r x y → r y x)
+variable (trans_r : ∀ {x y z}, r x y → r y z → r x z)
+
+example (a b c d : α) (hab : r a b) (hcb : r c b) (hcd : r c d) : r a d :=
+  trans_r (trans_r hab (symm_r hcb)) hcd
+
+-- Prop (Sort 0)  Type (Sort 1)  Type 1 (Sort 2)  Type 2 (Sort 3)
+#check Nat    -- Type
+#check Type   -- Type 1
+#check Sort 0 -- Type
+#check Prop   -- Type
