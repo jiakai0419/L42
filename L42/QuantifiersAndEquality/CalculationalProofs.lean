b2c7dabe7a284615a2fea05bc2b65cdb04aba87f
Kai Jia <jiakai0419@gmail.com>
2024-10-01 20:54:18 +0800

    Update: QuantifiersAndEquality/CalculationalProofs

--- L42/QuantifiersAndEquality/CalculationalProofs.lean
+++ L42/QuantifiersAndEquality/CalculationalProofs.lean
@@ -91,3 +91,53 @@ example
     _ ≤ c     := h2
     _ < c + 1 := Nat.lt_succ_self c
     _ < d     := h3
+
+
+def divides (x y : Nat) : Prop :=
+  ∃ k, k*x = y
+
+def divides_trans (h₁ : divides x y) (h₂ : divides y z) : divides x z :=
+  let ⟨k₁, d₁⟩ := h₁
+  let ⟨k₂, d₂⟩ := h₂
+  ⟨k₁ * k₂, by rw [Nat.mul_comm k₁ k₂, Nat.mul_assoc, d₁, d₂]⟩
+
+def divides_mul (x : Nat) (k : Nat) : divides x (k*x) :=
+  ⟨k, rfl⟩
+
+instance : Trans divides divides divides where
+  trans := divides_trans
+
+example (h₁ : divides x y) (h₂ : y = z) : divides x (2*z) :=
+  calc
+    divides x y     := h₁
+    _ = z           := h₂
+    divides _ (2*z) := divides_mul z 2
+
+infix:50 " ∣∣ " => divides
+
+example (h₁ : divides x y) (h₂ : y = z) : divides x (2*z) :=
+  calc
+    x ∣∣ y     := h₁
+    _ = z     := h₂
+    _ ∣∣ 2*z   := divides_mul z 2
+
+
+example (x y : Nat) : (x + y) * (x + y) = x * x + y * x + x * y + y * y :=
+  calc
+    (x + y) * (x + y) = (x + y) * x + (x + y) * y   := by rw [Nat.mul_add]
+    _ = x * x + y * x + (x + y) * y                 := by rw [Nat.add_mul]
+    _ = x * x + y * x + (x * y + y * y)             := by rw [Nat.add_mul]
+    _ = x * x + y * x + x * y + y * y               := by rw [←Nat.add_assoc]
+
+example (x y : Nat) : (x + y) * (x + y) = x * x + y * x + x * y + y * y :=
+  calc (x + y) * (x + y)
+    _ = (x + y) * x + (x + y) * y       := by rw [Nat.mul_add]
+    _ = x * x + y * x + (x + y) * y     := by rw [Nat.add_mul]
+    _ = x * x + y * x + (x * y + y * y) := by rw [Nat.add_mul]
+    _ = x * x + y * x + x * y + y * y   := by rw [←Nat.add_assoc]
+
+example (x y : Nat) : (x + y) * (x + y) = x * x + y * x + x * y + y * y :=
+  by rw [Nat.mul_add, Nat.add_mul, Nat.add_mul, ←Nat.add_assoc]
+
+example (x y : Nat) : (x + y) * (x + y) = x * x + y * x + x * y + y * y :=
+  by simp [Nat.mul_add, Nat.add_mul, Nat.add_assoc]
