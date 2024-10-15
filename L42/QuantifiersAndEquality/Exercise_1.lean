51e7c0d6a28eee17c52387efa955b47b14d9bfbd
Kai Jia <jiakai0419@gmail.com>
2024-10-15 15:05:27 +0800

    Update: QuantifiersAndEquality/Exercise_1

--- L42/QuantifiersAndEquality/Exercise_1.lean
+++ L42/QuantifiersAndEquality/Exercise_1.lean
@@ -6,3 +6,18 @@ example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) :=
     ⟨λ x : α => (h x).left, λ x : α => (h x).right⟩)
   (λ h : (∀ x, p x) ∧ (∀ x, q x) =>
     λ x : α => ⟨h.left x, h.right x⟩)
+
+example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) :=
+  λ hpq : (∀ x, p x → q x) =>
+  λ hp : (∀ x, p x) =>
+  λ x : α =>
+    (hpq x) (hp x)
+
+example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x :=
+  λ h : (∀ x, p x) ∨ (∀ x, q x) =>
+  λ x : α =>
+    h.elim
+      (λ hp : (∀ x, p x) => Or.inl (hp x))
+      (λ hq : (∀ x, q x) => Or.inr (hq x))
+
+-- You should also try to understand why the reverse implication is not derivable in the last example.
