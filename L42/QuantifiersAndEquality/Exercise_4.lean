c60a776026571d4cb91da50e730e3bff17cb9fe2
Kai Jia <jiakai0419@gmail.com>
2024-11-13 14:36:44 +0800

    Update: QuantifiersAndEquality/Exercise_4

--- L42/QuantifiersAndEquality/Exercise_4.lean
+++ L42/QuantifiersAndEquality/Exercise_4.lean
@@ -3,3 +3,10 @@ def even (n : Nat) : Prop :=
 
 def prime (n : Nat) : Prop :=
   n > 1 ∧ ∀ m : Nat, 1 < m ∧ m < n → n % m ≠ 0
+
+def infinitely_many_primes : Prop := sorry
+
+def Fermat_prime (n : Nat) : Prop :=
+  prime n ∧ ∃ k : Nat, 0 ≤ k ∧ 2 ^ (2 ^ k) + 1 = n
+
+def infinitely_many_Fermat_primes : Prop := sorry
